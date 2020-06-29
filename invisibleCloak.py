45b54e0a078b29bf760700d1ccd887ff30db1d73
Vinit Soni <vinitsoni0728@gmail.com>
2020-06-29 19:15:09 +0530

    Final

---
+++ invisibleCloak.py
@@ -0,0 +1,66 @@
+# Importing required libraries
+import cv2
+import time
+import numpy as np
+
+# Preparation for writing the output video
+video = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', video, 20.0, (640, 480))
+
+# Capturing the initial frame from the webCam
+cap = cv2.VideoCapture(0)
+
+# Allowing the system to sleep for 3 seconds before the webCam starts
+time.sleep(3)
+count = 0
+background = 0
+
+# Capturing the background in range of 60
+for i in range(60):
+    ret, background = cap.read()
+background = np.flip(background, axis=1)
+
+# Reading every frame from the webCam, until the camera is open
+while cap.isOpened():
+    ret, img = cap.read()
+    if not ret:
+        break
+    count += 1
+    img = np.flip(img, axis=1)
+
+    # Converting the color space from BGR to HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # Generating masks to detect red color in the frame
+    lower_red = np.array([0, 120, 70])
+    upper_red = np.array([10, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+    lower_red = np.array([170, 120, 70])
+    upper_red = np.array([180, 255, 255])
+    mask2 = cv2.inRange(hsv, lower_red, upper_red)
+
+    mask1 = mask1 + mask2
+
+    # Opening and Dilating the mask image
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
+
+    # Creating an inverted mask to segment out the red color from the frame
+    mask2 = cv2.bitwise_not(mask1)
+
+    # Segmenting the red color part out of the frame using bitwise and with the inverted mask
+    res1 = cv2.bitwise_and(img, img, mask=mask2)
+
+    # Creating image showing static background frame pixels only for the masked region
+    res2 = cv2.bitwise_and(background, background, mask=mask1)
+
+    # Generating the final output
+    finalOutput = cv2.addWeighted(res1, 1, res2, 1, 0)
+    out.write(finalOutput)
+    cv2.imshow("magic", finalOutput)
+    cv2.waitKey(1)
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
